79027cce21e62a897d6aa639d7fdb3d797b42740
KWONZIPSA <ultrafood@naver.com>
2024-04-24 23:22:53 +0900

    install pug

--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ const app = express();
 const logger = morgan("dev"); // 스크립트 이름(여기서는 dev)쓰는 방식, combined 방식, common, short, tiny등이 있음.
 app.use(logger); //morgan을 쓰면 보다 정교한 로깅이 가능.
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
