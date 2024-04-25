ec44f06be22d174aef37996a8a0ed2d02c8ca918
KWONZIPSA <ultrafood@naver.com>
2024-04-25 22:31:26 +0900

    variables to templates
    #{변수}

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,11 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const see = (req, res) => {
   console.log(req.params);
-  res.render("watch");
+  res.render("watch", { pageTitle: "Watch" });
 };
 export const edit = (req, res) => {
   console.log(req.params);
-  res.render("edit");
+  res.render("edit", { pageTitle: "Edit" });
 };
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
