eb48d0a5c3e8c0fc54414102c803a7ca8d587fe4
KWONZIPSA <ultrafood@naver.com>
2024-04-29 23:07:51 +0900

    add mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,23 +3,44 @@ const fakeUser = {
   loggedIn: true,
 };
 
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "Hello",
-    },
-    {
-      title: "Video #2",
-    },
-    {
-      title: "Whatsup",
-    },
-  ];
   return res.render("home", { pageTitle: "Home", fakeUser, videos });
 };
 export const see = (req, res) => {
-  console.log(req.params);
-  res.render("watch", { pageTitle: "Watch" });
+  const { id } = req.params;
+  const video1 = videos[id - 1];
+  console.log("Show video", id);
+  return res.render("watch", {
+    pageTitle: `Watching ${video1.title}`,
+    fakeUser,
+  });
 };
 export const edit = (req, res) => {
   console.log(req.params);
