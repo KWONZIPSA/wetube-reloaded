458501971e958fe06c472ae58901eb7fb0445638
KWONZIPSA <ultrafood@naver.com>
2024-05-08 22:58:31 +0900

    more practice and install mongoDB

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
 import {
   watch,
-  deleteVideo,
-  upload,
   getEdit,
   postEdit,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -14,6 +14,7 @@ const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch); //id일필요 없고 콜론(:)이 중요함. 파라미터임.
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 //videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 //videoRouter.get("/upload", upload); //파라미터보다 위에 와야지 파라미터로 인식 안됨. 하지만 파라미터(:)를 정규식으로 만들면 순서가 상관 없음.
 
