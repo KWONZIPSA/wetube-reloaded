cf95a77812d79c0afcbab74de620789ecd067245
KWONZIPSA <ultrafood@naver.com>
2024-05-16 23:43:56 +0900

    create DB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -21,10 +21,7 @@ import Video from "../models/Video";
     */
 
 export const home = async (req, res) => {
-  console.log("i start");
   const videos = await Video.find({});
-  console.log(videos);
-  console.log("i finished");
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
@@ -43,7 +40,20 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
-  const { title } = req.body;
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  //const video = new Video({ }) ~~~   await video.save(); 이렇게 해도 되고 아래 처럼 await Video.create({});라고 해도 돼.
+  await Video.create({
+    title: title, //앞의 title은 스키마의 title, 뒤의 title은 req.body 안의 title 의미. 이름이 같아 축약형으로 title이라고 쓸 수 있음.
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`), //split 구분자로 잘라서 어레이로 만들어줌. map은 단어에 뭔가를 붙여줌.
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+    // String 항목은 숫자로 넣어도 문자로 치환, 숫자항목은 문자를 넣으면 없는 값으로 생성해서 오류를 최소화함
+  });
+
   return res.redirect("/");
 };
